edc65fa70731a5f0b614ed460d951f4631d16bb1
Hojune Yoo <diogenes0803@gmail.com>
2022-06-01 19:50:18 -0700

    userseed add

--- 11kr_server/database/seeders/DatabaseSeeder.php
+++ 11kr_server/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\BlackListSeed;
+use Database\Seeders\UserSeed;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BlackListSeed::class,
+            UserSeed::class,
         ]);
     }
 }
