1e05c80946c7ab5a8a777ea0be882634a290847d
Hojune Yoo <diogenes0803@gmail.com>
2022-06-09 12:17:28 -0700

    missing library added

--- 11kr_server/app/Http/Controllers/UserController.php
+++ 11kr_server/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Http;
 use App\Models\User;
 
 class UserController extends Controller
